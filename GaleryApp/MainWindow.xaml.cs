73e241e6a9a5a56336cb8ba0e37a2eaebae1c434
hasanofdev <hesenovelsad468@gmail.com>
2022-11-29 17:56:13 +0400

    Uptade!

--- GaleryApp/MainWindow.xaml.cs
+++ GaleryApp/MainWindow.xaml.cs
@@ -11,40 +11,60 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Drawing;
+using Point = System.Windows.Point;
+using Image = System.Drawing.Image;
+using System.Windows.Media.Imaging;
+using GaleryApp.Pages;
 
 namespace GaleryApp
 {
     public partial class MainWindow : Window
     {
-        List<Image> images;
         public MainWindow()
         {
             InitializeComponent();
-            images = new List<Image>();
         }
 
-        private void SaveBtn_Click(object sender, RoutedEventArgs e)
+        public void CaptureScreen(UIElement source, Uri destination)
         {
-            string path = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
-            string fileName = "image";
-
-            int imageNumber = 0;
-            while (File.Exists(path + "fileName"))
+            try
             {
-                if (int.TryParse(fileName[fileName.Length - 1].ToString(), out imageNumber))
-                    fileName += imageNumber.ToString();
-                else
-                    break;
+                double Height, renderHeight, Width, renderWidth;
 
+                Height = renderHeight = source.RenderSize.Height;
+                Width = renderWidth = source.RenderSize.Width;
+
+                //Specification for target bitmap like width/height pixel etc.
+                RenderTargetBitmap renderTarget = new RenderTargetBitmap((int)renderWidth, (int)renderHeight, 96, 96, PixelFormats.Pbgra32);
+                //creates Visual Brush of UIElement
+                VisualBrush visualBrush = new VisualBrush(source);
+
+                DrawingVisual drawingVisual = new DrawingVisual();
+                using (DrawingContext drawingContext = drawingVisual.RenderOpen())
+                {
+                    //draws image of element
+                    drawingContext.DrawRectangle(visualBrush, null, new Rect(new Point(0, 0), new Point(Width, Height)));
+                }
+                //renders image
+                renderTarget.Render(drawingVisual);
+
+                //PNG encoder for creating PNG file
+                PngBitmapEncoder encoder = new PngBitmapEncoder();
+                encoder.Frames.Add(BitmapFrame.Create(renderTarget));
+                using (FileStream stream = new FileStream(destination.LocalPath, FileMode.Create, FileAccess.Write))
+                {
+                    encoder.Save(stream);
+                }
             }
-
-            Image image = new Image();
-            var stream = new MemoryStream();
-            image.SetBinding
-
+            catch (Exception e)
+            {
+                MessageBox.Show(e.ToString());
+            }
         }
+
+       
     }
 }
